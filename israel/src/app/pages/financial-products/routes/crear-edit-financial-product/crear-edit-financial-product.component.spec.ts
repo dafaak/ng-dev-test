a5aefaa48f36fe934b363aa448e9ec54908d4ae3
Israel <israel.pe@outlook.es>
2024-07-25 18:13:49 -0500

    featt: cambios

--- israel/src/app/pages/financial-products/routes/crear-edit-financial-product/crear-edit-financial-product.component.spec.ts
+++ israel/src/app/pages/financial-products/routes/crear-edit-financial-product/crear-edit-financial-product.component.spec.ts
@@ -1,18 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CrearEditFinancialProductComponent } from './crear-edit-financial-product.component';
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { Observable, of } from "rxjs";
+import { FinancialProductInterface } from "../../../../models";
+import { FinancialProductHttpService } from "../../../../services";
+import { ActivatedRoute, Router } from "@angular/router";
+
+class FakeFinancialProductsService {
+  getFinancialProducts(): Observable<FinancialProductInterface[]> {
+    return of([])
+  }
+}
+
+class FakeRouter {
+  navigate(params: string[]) {
+  }
+}
+
+class FakeActivatedRoute {
+  snapshot: {params:{id:string}}={params:{id:'2'}};
+}
 
 describe('CrearEditFinancialProductComponent', () => {
   let component: CrearEditFinancialProductComponent;
   let fixture: ComponentFixture<CrearEditFinancialProductComponent>;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CrearEditFinancialProductComponent]
+      imports: [CrearEditFinancialProductComponent,
+
+      ],
+      providers: [
+        {
+          provide: FinancialProductHttpService, useClass: FakeFinancialProductsService
+        },
+        {provide: Router, useClass: FakeRouter},
+        {provide: ActivatedRoute, useClass: FakeActivatedRoute},
+        provideHttpClientTesting()
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CrearEditFinancialProductComponent);
+    router = TestBed.inject(Router);
+    activatedRoute = TestBed.inject(ActivatedRoute);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
